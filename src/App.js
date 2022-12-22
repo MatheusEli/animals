6e9c55c087abaaf5095c0d87679f7169a481dfa6
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2022-12-22 15:45:18 -0300

    handleClick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import AnimalShow from "./AnimalShow";
+import { useState } from "react";
 
 function App() {
+
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        
+        setCount(count + 1);
+    }
+
     return (
         <div>
-            <button>add Animal</button>
-            <p>Animals to show: 0</p>
+            <button onClick={handleClick}>Add Animal</button>
+            <p>Animals to show: {count}</p>
         </div>
     );
 }
