66591dbb546daec7178c1252c8b3d9c1f434ad8c
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2022-12-26 17:46:07 -0300

    list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import AnimalShow from "./AnimalShow";
 import { useState } from "react";
+import './App.css';
+
+function getRandomAnimal(){
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[ Math.floor(Math.random()*animals.length) ];
+}
+
 
 function App() {
-
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
 
     const handleClick = () => {
-        
-        setCount(count + 1);
+        setAnimals([...animals, getRandomAnimal()])
     }
 
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    })
+
     return (
-        <div>
+        <div className="app">
             <button onClick={handleClick}>Add Animal</button>
-            <p>Animals to show: {count}</p>
+            <div className="animal-list">{renderedAnimals}</div>
         </div>
     );
 }
